77c9e2e603f467ee3eba6d304cb1e34d757b7b85
adrian <adrianbravo145@gmail.com>
2019-09-07 21:19:48 -0430

    Primer commit angular avanzado

---
+++ src/app/shared/breadcumbs/breadcumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumbs',
+  templateUrl: './breadcumbs.component.html',
+  styleUrls: ['./breadcumbs.component.scss']
+})
+export class BreadcumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
